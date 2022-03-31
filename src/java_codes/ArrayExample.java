f973153e4aedc5b24963001e55af4853a703b3fe
madinashukurova <mshukurova85@gmail.com>
2022-03-30 21:19:23 -0400

    New code

---
+++ src/java_codes/ArrayExample.java
@@ -0,0 +1,11 @@
+package java_codes;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int [] nums = {3, 10, 5, 8, 6, 9};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
